7b320b08e665656efbd37f96daafb96dcc153de1
Ndisyamwende <sharonndisya2002@gmail.com>
2023-12-17 23:50:55 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(names, occasion) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`);
+    }
+    return thankYouMessages;
+  } 
+  
+  function countDown(startingNumber) {
+    for (let i = startingNumber; i >= 0; i--) {
+      console.log(i);
+    }
+  }
